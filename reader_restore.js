e5720d943379838fd91cab2084bce2cb9bcb5314
KevBaister <kevbaister+code@gmail.com>
2012-05-17 11:52:13 +0100

    Updated jQuery and changed the plugin to poll for the navigation bar if it isn't available, making the restoration more reliable.

--- reader_restore.js
+++ reader_restore.js
@@ -1,9 +1,25 @@
 $(function(){
   $('#canvas_frame').ready(function() {
-    $('#canvas_frame').contents().find('#gbz ol.gbtc li.gbt').last().before($('#canvas_frame').contents().find('#gb_32').parent('li'));
-    $('#canvas_frame').contents().find('#gb_32').parent('li').removeClass('gbmtc').addClass('gbt');
-    var $readerLink = $('#canvas_frame').contents().find('#gb_32');
-    var linkText = $readerLink.text();
-    $readerLink.removeClass('gbmt').addClass('gbzt').html('<span class="gbtb2"></span><span class="gbts">' +  linkText + '</span>');
+
+    if($('#canvas_frame').contents().find('#gbz').length !== 0){
+      restoreReader();
+    } else {
+      var interval;
+      interval = setInterval(function() {
+        if($('#canvas_frame').contents().find('#gbz').length !== 0) {
+          clearInterval(interval);
+          restoreReader();
+        }
+      }, 500);
+    }
+
   });
 });
+
+function restoreReader(){
+  $('#canvas_frame').contents().find('#gbz ol.gbtc li.gbt').last().before($('#canvas_frame').contents().find('#gb_32').parent('li'));
+  $('#canvas_frame').contents().find('#gb_32').parent('li').removeClass('gbmtc').addClass('gbt');
+  var $readerLink = $('#canvas_frame').contents().find('#gb_32');
+  var linkText = $readerLink.text();
+  $readerLink.removeClass('gbmt').addClass('gbzt').html('<span class="gbtb2"></span><span class="gbts">' +  linkText + '</span>');
+}
